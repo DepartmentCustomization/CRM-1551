21c6340c71a87cc206ea176eab42887146b53c9e
artemkov1994 <artem_kov@email.ua>
2019-12-11 17:57:05 +0200

    добавил_форму_секцию

--- Configuration/DataSources/System.Data.SqlClient/CRM_1551_Hotline1500/All_CRM_1551_Hotline1500/Hotline/ak_hotl_AppealsSRow/ak_hotl_AppealsSRow.sql
+++ Configuration/DataSources/System.Data.SqlClient/CRM_1551_Hotline1500/All_CRM_1551_Hotline1500/Hotline/ak_hotl_AppealsSRow/ak_hotl_AppealsSRow.sql
@@ -1,4 +1,4 @@
-select a.Id, a.registration_date, a.phone_number, 
+select a.Id, a.registration_date, a.phone_number, a.applicant_needs,
   a.applicant_name, a.applicant_address, a.marital_status_id, a.sex, a.age,
   a.education_id, a.applicant_privilage_id, a.guidance_kind_id,
   a.offender_name, a.service_content, a.comment
