d9ef08b9001207696ba5ae287b1522e21b9b8013
MarkusEfre <markus.efr@gmail.com>
2019-12-21 15:52:23 +0200

    Форма заявителя - настроил управление телефонами

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Applicants/cx_Applicants_SelectsRows/cx_Applicants_SelectsRows.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Applicants/cx_Applicants_SelectsRows/cx_Applicants_SelectsRows.sql
@@ -1,63 +1,73 @@
+----------------> GET APPLICANT PHONES IN ROW <-----------------
+IF OBJECT_ID('tempdb.dbo.#ApplicantPhones') IS NOT NULL
+BEGIN
+	DROP TABLE #ApplicantPhones
+END
 
-select [Applicants].Id, 
+SELECT DISTINCT
+	  applicant_id
+	, phone_number = CAST(NULL AS VARCHAR(500))
+INTO #ApplicantPhones
+FROM dbo.ApplicantPhones
+
+DECLARE
+	  @applicantID INT
+	, @Value CHAR(10)
+
+DECLARE cur CURSOR LOCAL READ_ONLY FAST_FORWARD FOR
+    SELECT
+	      applicant_id
+	     ,phone_number
+    FROM dbo.ApplicantPhones
+
+OPEN cur
+FETCH NEXT FROM cur INTO
+	  @applicantID
+	, @Value
+
+WHILE @@FETCH_STATUS = 0 BEGIN
+
+    UPDATE #ApplicantPhones
+    SET 
+          phone_number = ISNULL(phone_number + ', ' + @Value, @Value)
+    WHERE applicant_id = @applicantID
+
+	FETCH NEXT FROM cur INTO
+          @applicantID
+        , @Value
+END
+
+CLOSE cur
+DEALLOCATE cur
+---------------------> END GETTING PHONES <---------------------
+
+select  
+[Applicants].Id, 
 [Applicants].full_name, 
-ApplicantPhonesMain.phone_number, 
-ApplicantPhonesMain.PhoneType PhoneType, 
-ApplicantPhonesNotMain.phone_number phone_number2, 
-ApplicantPhonesNotMain.PhoneType PhoneType2, 
 [Applicants].mail,
 [Districts].name DistrictsName, 
---concat(StreetTypes.shortname,' '+ [Streets].[name], ', буд. '+ ltrim(Buildings.number), '' + Buildings.letter) as BuildNumber,
  concat(StreetTypes.shortname,' ', [Streets].[name]) Street,
-  concat(Buildings.number,Buildings.letter) BuildNumber,
-
+ concat(Buildings.number,Buildings.letter) BuildNumber,
 [LiveAddress].house_block, 
 [LiveAddress].entrance, 
 [LiveAddress].flat,
- [ApplicantTypes].name ApplicantType, 
- [CategoryType].name Category, 
- [ApplicantPrivilege].Name Privilege, 
- [SocialStates].name [SocialStates], 
- [Applicants].sex, 
+[ApplicantTypes].name ApplicantType, 
+[CategoryType].name Category, 
+[ApplicantPrivilege].Name Privilege, 
+[SocialStates].name [SocialStates], 
+[Applicants].sex, 
 
  case when [Applicants].birth_date is null then convert(nvarchar(200),[Applicants].birth_year) else convert(nvarchar(200),[Applicants].birth_date) end birth_date,
-
-
  case 
   when month(convert(date, [Applicants].birth_date))<=month(getdate())
   and day(convert(date, [Applicants].birth_date))<=day(getdate())
   then DATEDIFF(yy, convert(date, [Applicants].birth_date), getdate())
   else DATEDIFF(yy, convert(date, [Applicants].birth_date), getdate())-1 end age, 
  [Applicants].comment,
- [ApplicantPhonesMain].phone_number_norm,
- [ApplicantPhonesNotMain].phone_number_norm phone_number_norm2
+ #ApplicantPhones.phone_number
 
   from [dbo].[Applicants]
-  left join 
-  (select [ApplicantPhones].[Id]
-      ,N'('+left([phone_number],3)+N')'+substring([phone_number],4,3)+N'-'+substring([phone_number],7,2)+N'-'+substring([phone_number],9,2) [phone_number]
-	  ,[ApplicantPhones].[applicant_id]
-	  ,[PhoneTypes].name PhoneType
-	  ,[phone_number] [phone_number_norm]
-from [CRM_1551_Analitics].[dbo].[ApplicantPhones]
-left join [CRM_1551_Analitics].[dbo].[PhoneTypes] on [ApplicantPhones].phone_type_id=[PhoneTypes].Id
-where [ApplicantPhones].[IsMain]=N'true') ApplicantPhonesMain on [Applicants].Id=[ApplicantPhonesMain].applicant_id
-  
-  left join 
-
-  (select [ApplicantPhones].[Id]
-      ,N'('+left([phone_number],3)+N')'+substring([phone_number],4,3)+N'-'+substring([phone_number],7,2)+N'-'+substring([phone_number],9,2) [phone_number]
-	  ,[ApplicantPhones].[applicant_id]
-	  ,[PhoneTypes].name PhoneType
-	  ,[phone_number] [phone_number_norm]
-from [CRM_1551_Analitics].[dbo].[ApplicantPhones]
-left join [CRM_1551_Analitics].[dbo].[PhoneTypes] on [ApplicantPhones].phone_type_id=[PhoneTypes].Id
-where [ApplicantPhones].Id in (select max(Id) max_id
-  from [CRM_1551_Analitics].[dbo].[ApplicantPhones]
-  where IsMain=N'false'
-  group by [applicant_id])) ApplicantPhonesNotMain on [Applicants].Id=[ApplicantPhonesNotMain].applicant_id
-
-  --left join [dbo].[PhoneTypes] on [ApplicantPhones].phone_type_id=[PhoneTypes].Id
+  left join #ApplicantPhones on #ApplicantPhones.applicant_id = [Applicants].Id 
   left join [dbo].[LiveAddress] on [Applicants].Id=[LiveAddress].[applicant_id]
   left join [dbo].[Buildings] on [LiveAddress].building_id=[Buildings].Id
   left join [dbo].[Districts] on [Buildings].district_id=Districts.Id
@@ -67,9 +77,7 @@ where [ApplicantPhones].Id in (select max(Id) max_id
   left join [dbo].[CategoryType] on [Applicants].category_type_id=[CategoryType].Id
   left join [dbo].[ApplicantPrivilege] on [Applicants].applicant_privilage_id=[ApplicantPrivilege].Id
   left join [dbo].[SocialStates] on [Applicants].social_state_id=[SocialStates].Id
-
- 
-  where #filter_columns#
---   order by [Applicants].[registration_date]
+  where 
+     #filter_columns#
      #sort_columns#
  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
