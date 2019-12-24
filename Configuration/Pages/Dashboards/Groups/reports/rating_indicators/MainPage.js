5ef54a777bde5272329b1d1c46ed70eea0a94c7d
Yehor <egorkatornado@gmail.com>
2019-12-24 16:04:34 +0200

    Промежуточный коммит

--- Configuration/Pages/Dashboards/Groups/reports/rating_indicators/MainPage.js
+++ Configuration/Pages/Dashboards/Groups/reports/rating_indicators/MainPage.js
@@ -1,24 +1,26 @@
 (function () {
-  return { 
+  return {
     init: function() {
-      this.sub = this.messageService.subscribe( 'GlobalFilterChanged', this.getFiltersParams, this );
+        this.sub3 = this.messageService.subscribe('GlobalFilterChanged', this.getFilterParams, this);
     },
-    getFiltersParams: function(message){
-      const dateFilter = message.package.value.values.find(f => f.name === 'period').value;
-      const executorFilter = message.package.value.values.find(f => f.name === 'executor').value;
-      const ratingFilter = message.package.value.values.find(f => f.name === 'rating').value;
-      const name = 'FiltersParams';
-      const date = this.changeDateTimeValues(dateFilter);
-      if( date !== '' ){
-        const executor = executorFilter === null ? 0 :  executorFilter === '' ? 0 : executorFilter.value;
-        const rating = ratingFilter === null ? 0 :  ratingFilter === '' ? 0 : ratingFilter.value;
-        this.messageService.publish({ name, date, executor, rating  });
-      }
+
+    getFilterParams: function(message) {
+        const dateFilter = message.package.value.values.find(f => f.name === 'period').value;
+        const executorFilter = message.package.value.values.find(f => f.name === 'executor').value;
+        const ratingFilter = message.package.value.values.find(f => f.name === 'rating').value;
+        const name = 'FiltersParams';
+        const date = this.changeDateTimeValues(dateFilter);
+        debugger;
+        if( date !== '' ){
+            const executor = executorFilter === null ? 0 :  executorFilter === '' ? 0 : executorFilter.value;
+            const rating = ratingFilter === null ? 0 :  ratingFilter === '' ? 0 : ratingFilter.value;
+            this.messageService.publish({ name, date, executor, rating  });
+        }
     },
-    changeDateTimeValues: function(value){
-      
+
+    changeDateTimeValues: function(value) {
         if( value === '') {
-          return value;
+            return value;
         }
         let date = new Date(value);
         let dd = date.getDate().toString();
@@ -28,8 +30,9 @@
         mm = mm.length === 1 ? '0' + mm : mm;
         return yyyy + '-' + mm + '-' + dd;
     },
+
     destroy: function () {
-      this.sub.unsubscribe();
+        this.sub3.unsubscribe();
     }
-  };
+};
 }());
